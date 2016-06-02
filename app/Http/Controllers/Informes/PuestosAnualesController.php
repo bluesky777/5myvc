6698aa0ce1e1829ffc4bc7f64cb4593ceb959f35
Joseth David <davidguerrero777@gmail.com>
2016-06-02 17:11:10 -0500

    Mejorando puestos y boletin popup imagen

--- app/Http/Controllers/Informes/PuestosAnualesController.php
+++ app/Http/Controllers/Informes/PuestosAnualesController.php
@@ -92,7 +92,7 @@ class PuestosAnualesController extends Controller {
 				(
 				SELECT R1.materia, R1.alias, SUM(valor_nota) as nota_asignatura, R1.periodo_id, R1.asignatura_id,
 					SUM(R1.perdido) as perdidos, R1.alumno_id,
-					R1.profesor_id, R1.nombres_profesor, R1.apellidos_profesor, R1.sexo, R1.foto_id, R1.foto_nombre
+					R1.profesor_id, R1.nombres_profesor, R1.apellidos_profesor, R1.sexo, R1.foto_id, R1.foto_nombre, R1.grupo_id
 			    FROM
 					(
 			        SELECT m.materia, m.alias, n.id as nota_id, n.nota, n.subunidad_id, n.alumno_id, 
@@ -100,7 +100,8 @@ class PuestosAnualesController extends Controller {
 			            IF(n.nota < ?, 1, 0) as perdido, 
 						s.definicion, s.porcentaje as porc_subuni, s.unidad_id, u.porcentaje as porc_uni, u.periodo_id, u.asignatura_id,
 						pr.id as profesor_id, pr.nombres as nombres_profesor, pr.apellidos as apellidos_profesor, pr.sexo,
-						pr.foto_id, IFNULL(i.nombre, IF(pr.sexo="F","default_female.jpg", "default_male.jpg")) as foto_nombre 
+						pr.foto_id, IFNULL(i.nombre, IF(pr.sexo="F","default_female.jpg", "default_male.jpg")) as foto_nombre,
+						a.grupo_id 
 			        FROM notas n 
 						inner join subunidades s on s.id=n.subunidad_id and s.deleted_at is null
 						inner join unidades u on u.id=s.unidad_id and u.deleted_at is null
@@ -114,6 +115,8 @@ class PuestosAnualesController extends Controller {
 			        )R1
 				GROUP BY R1.asignatura_id, R1.periodo_id
 			    )R2
+                inner join grupos g on g.id=R2.grupo_id
+                inner join matriculas m on m.grupo_id=g.id and R2.alumno_id=m.alumno_id and m.deleted_at is null
 			GROUP BY R2.asignatura_id';
 
 		$notas = DB::select($consulta, [
