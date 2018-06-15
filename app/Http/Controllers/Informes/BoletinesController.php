bf2159bab3633587dfef29a0d5cf970de8e1d4a6
Joseth Guerrero <davidguerrero777@gmail.com>
2018-06-15 10:03:55 -0500

    if EscalaDeValoracion

--- app/Http/Controllers/Informes/BoletinesController.php
+++ app/Http/Controllers/Informes/BoletinesController.php
@@ -196,7 +196,11 @@ class BoletinesController extends Controller {
 			$alumno->promedio = $sumatoria_asignaturas / count($alumno->asignaturas);
 		}
 		
-		$alumno->promedio_desempenio = EscalaDeValoracion::valoracion($alumno->promedio, $this->escalas_val)->desempenio;
+		$des = EscalaDeValoracion::valoracion($alumno->promedio, $this->escalas_val);
+		
+		if ($des) {
+			$alumno->promedio_desempenio = $des->desempenio;
+		} 
 			
 
 
